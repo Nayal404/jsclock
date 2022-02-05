dc42c68f3a1ba3c96ea37912651dc83b7859d8ca
Nayal404 <93335142+Nayal404@users.noreply.github.com>
2022-02-05 11:26:24 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// console.log('Your JavaScript is running');
+console.log('Om/Nayal Sunar Browser Successfully executed JavaScript');
 let newd;
 let time;
 setInterval(() => {
    newd = new Date();
    time = newd.getHours()+":"+newd.getMinutes()+":"+newd.getSeconds();
-   document.getElementById('realtime').innerHTML = time; 
-   document.getElementById('fulltime').innerHTML = newd;
+   document.getElementById('realtime').innerHTML = time;
 }, 1000);
+function browseft(){
+    setInterval(() => {
+    document.getElementById('fulltime').innerHTML = "Full time of your Location:<br>"+newd;
+    }, 1000);
+}
